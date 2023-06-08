0f2adc87359c8aa02894e39552527507eb491dff
jose luis cebrian dominguez <cebrian.pepe@gmail.com>
2023-06-08 19:40:47 +0200

    Update muesting.r

--- R/muesting.r
+++ R/muesting.r
@@ -103,7 +103,7 @@ muesting <- function(x,y) {
 
   TALLAS<-tallas2[,c( "CALADERO_DCF",   "COD_TIPO_MUE", "COD_ID","FECHA", "QUARTER", "ESTRATO_RIM","PUERTO","BARCO", "TAXON",
                       "ESPECIE", "TALLA_MEDIA_MAREA", "EJEM_MED_MAREA",
-                      "PESO_SP")]%>%distinct()%>%as.data.frame()
+                      "PESO_SP")]%>%unique()%>%as.data.table()
 return(y)
   
   
