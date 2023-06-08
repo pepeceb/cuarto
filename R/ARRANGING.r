8fa7e2d0ab02705d524d231ee46859f7b98cdf9a
jose luis cebrian dominguez <cebrian.pepe@gmail.com>
2023-06-08 10:03:32 +0200

    Update ARRANGING.r

--- R/ARRANGING.r
+++ R/ARRANGING.r
@@ -61,7 +61,7 @@ arrange_sample <- function(x, y) {
                    "MUEST_SP_CAT", "PESO_SP_CAT", "MUEST_SP", "PESO_SP", "PESO_SIRENO")]
   pesos1<-distinct(pesos1)
 
-  subset(pesos1, COD_ID== "202200022")%>%as.data.frame()
+  #subset(pesos1, COD_ID== "202200022")%>%as.data.frame()
   tallas1<-distinct(tallas[,c("CALADERO_DCF",   "COD_ID","FECHA", "QUARTER",  "ESTRATO_RIM","PUERTO", "COD_TIPO_MUE",
                               "ESP_MUE","CATEGORIA", "ESP_CAT","TALLA", "EJEM_MEDIDOS")])
 
