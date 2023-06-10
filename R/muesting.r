7efc66fbe559feca0d52f21b97b9bcf85d07d21c
jose luis cebrian dominguez <cebrian.pepe@gmail.com>
2023-06-10 23:27:58 +0200

    Update muesting.r

--- R/muesting.r
+++ R/muesting.r
@@ -2,12 +2,9 @@
 
 library(stringi)    
  data=muestreos_tallas
-  muestreos_tallas$PUERTO<-toupper(stri_trans_general(muestreos_tallas$PUERTO,"Latin-ASCII"))
-  
-  
+  muestreos_tallas$PUERTO<-toupper(stri_trans_general(muestreos_tallas$PUERTO,"Latin-ASCII"))  
   
   muestreos_tallas<-data.table(muestreos_tallas)
-  head (muestreos_tallas)
   muestreos_tallas<-na.omit(muestreos_tallas, cols=c("EJEM_MEDIDOS", "SOP"))
   muestreos_tallas$FECHA_MUE<-as.character (muestreos_tallas$FECHA_MUE)
   muestreos_tallas$FECHA_MUE<-str_replace_all(muestreos_tallas$FECHA_MUE, "ENE", "JAN")
@@ -60,12 +57,11 @@ library(stringi)
   
   pesos1<-pesos[,c("COD_TIPO_MUE","COD_ID", "FECHA","QUARTER", "ESTRATO_RIM","PUERTO","BARCO",
                    "TAXON", "CATEGORIA", "ESPECIE",
-                   "MUEST_SP_CAT", "PESO_SP_CAT", "MUEST_SP", "PESO_SP", "PESO_SIRENO")]
-  pesos1<-distinct(pesos1)
+                   "MUEST_SP_CAT", "PESO_SP_CAT", "MUEST_SP", "PESO_SP", "PESO_SIRENO")]%>%unique()
 
 
-  tallas1<-distinct(tallas[,c("CALADERO_DCF",   "COD_ID","FECHA", "QUARTER",  "ESTRATO_RIM","PUERTO", "COD_TIPO_MUE",
-                              "ESP_MUE","CATEGORIA", "ESP_CAT","TALLA", "EJEM_MEDIDOS")])
+  tallas1<-tallas[,c("CALADERO_DCF",   "COD_ID","FECHA", "QUARTER",  "ESTRATO_RIM","PUERTO", "COD_TIPO_MUE",
+                              "ESP_MUE","CATEGORIA", "ESP_CAT","TALLA", "EJEM_MEDIDOS")]%>%unique()
   
   colnames(tallas1)[colnames(tallas1) %in% c("ESP_MUE", "ESP_CAT")] <- c("TAXON", "ESPECIE")
 
@@ -74,7 +70,7 @@ library(stringi)
     
   
   
-  tallas2<-full_join(pesos1, tallas1)%>%distinct()  %>%
+  tallas2<-full_join(pesos1, tallas1)%>%unique()  %>%
     group_by(COD_ID,TALLA, ESPECIE)%>%
     mutate(
       EJEM_POND_CAT= round((PESO_SP_CAT*EJEM_MEDIDOS/MUEST_SP_CAT),2)
@@ -84,24 +80,20 @@ library(stringi)
       EJEM_MED_TALLA=sum(EJEM_MEDIDOS),
       EJEM_POND_TALLA=sum(EJEM_POND_CAT),
       PESO_MUEST_TALLA= sum(MUEST_SP_CAT),
-      PESO_DESEM_TALLA = sum (PESO_SP_CAT),
-      EJEM_POND_METODOB= round((PESO_DESEM_TALLA*EJEM_MED_TALLA/PESO_MUEST_TALLA),2)
+      PESO_DESEM_TALLA = sum (PESO_SP_CAT)
     )  %>%ungroup()%>%
     group_by( COD_ID, ESPECIE)  %>%
     mutate(
       EJEM_MED_MAREA=sum(EJEM_MEDIDOS),
-      TALLA_MEDIA_MAREA= round (weighted.mean(TALLA, EJEM_POND_TALLA),2))%>%
-    ungroup()%>%
-    group_by(COD_ID, ESPECIE, CATEGORIA)%>%
-    mutate(TALLA_MEDIA_CAT=round (weighted.mean(TALLA, EJEM_POND_CAT),2))
+      TALLA_MEDIA_MAREA= round (weighted.mean(TALLA, EJEM_POND_TALLA),2))
   tallas2<-tallas2[complete.cases(tallas2[c("PESO_SP")]),]
  
   
   TALLAS<-tallas2[,c( "CALADERO_DCF",   "COD_TIPO_MUE", "COD_ID","FECHA", "QUARTER", "ESTRATO_RIM","PUERTO","BARCO", "TAXON",
                      "ESPECIE", "TALLA_MEDIA_MAREA", "EJEM_MED_MAREA",
-                     "PESO_SP")]%>% distinct()
+                     "PESO_SP")]%>% unique()
 
   
-write.csv(pesos, "pesos.csv", row.names = FALSE)
+write.csv(TALLAS, "pesos.csv", row.names = FALSE)
   return (TALLAS)
 } 
