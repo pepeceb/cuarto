84e956535d43d3ce9dcac5c3a85222d15a675e74
jose luis cebrian dominguez <cebrian.pepe@gmail.com>
2024-02-05 09:26:30 +0100

    Update cooking mn.r

--- cooking.r
+++ cooking.r
@@ -24,7 +24,7 @@ cooking <- function(AC, cooksd, PESO_SP, PUERTO2, ESPECIE, ESTRATO_RIM, sp3, MN,
     geom_hline(data = AC, aes(yintercept = 4*AC$MN),size=1.5, colour="red")  +
     guides(colour = guide_legend(override.aes = list(size = 3)))     +
     guides(scale= "none",size=FALSE,fill=guide_legend(override.aes=list(size=3))) +
-    scale_size(range=c(2,5))  +
+    scale_size(range=c(2,8))  +
     facet_wrap(ESPECIE~ESTRATO_RIM, scales="free")   +
     theme(plot.title = element_text(hjust=0.5,lineheight=7, face="bold", size=16),
           plot.subtitle = element_text(hjust=0.5,lineheight=10, face="bold.italic", 
@@ -43,8 +43,7 @@ cooking <- function(AC, cooksd, PESO_SP, PUERTO2, ESPECIE, ESTRATO_RIM, sp3, MN,
     geom_label_repel(show.legend=FALSE,data=subset(AC,cooksd>4*MN),aes(fontface="bold", 
                           TALLA_MEDIA_MAREA,cooksd, 
 label = ifelse(cooksd>4*MN,paste(round(TALLA_MEDIA_MAREA,2), "cm", "\n",
-                          FECHA, " ", "\n",EJEM_MED_MAREA, "Ejemplares"),"")  ,
-                              vjust=0, hjust=0.5))     +
+FECHA, " ", "\n",EJEM_MED_MAREA, "Ejemplares"),"")  , vjust=0, hjust=0.5))     +
     guides(colour = guide_legend(override.aes = list(size=5,linetype=4)))
   
 ggsave(temp_plot, file=paste0("2021_plot_TALLAS_MEDIAS ",AC$ESPECIE," ", ".png"), width = 35, height =25, units = "cm")
